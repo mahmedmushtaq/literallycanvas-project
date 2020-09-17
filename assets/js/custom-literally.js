2c5783e7efd9cc16615ed85fb2785c6652bdc2ef
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-09-17 18:12:51 +0500

    Complete literallcanvas project

---
+++ assets/js/custom-literally.js
@@ -0,0 +1,282 @@
+var lc = null;
+var tools;
+var strokeWidths;
+var colors;
+
+var setCurrentByName;
+var findByName;
+
+// the only LC-specific thing we have to do
+var containerOne = document.getElementsByClassName('literally')[0];
+
+const lcCanvas = () => {
+  return lc.getImage({
+    scale: 1,
+    margin: { top: 10, right: 10, bottom: 10, left: 10 },
+  });
+};
+
+function download() {
+  var download = document.getElementById('download');
+  var image = lcCanvas()
+    .toDataURL('image/png')
+    .replace('image/png', 'image/octet-stream');
+  download.setAttribute('href', image);
+}
+
+var showLC = function () {
+  lc = LC.init(containerOne, {
+    snapshot: JSON.parse(localStorage.getItem('drawing')),
+    defaultStrokeWidth: 10,
+    strokeWidths: [10, 20, 50],
+    secondaryColor: 'transparent',
+  });
+  window.demoLC = lc;
+
+  $('#add-img-btn').on('click', function () {
+    if (imgObj.src) {
+      // hide anchors
+      hideAnchors();
+      const canvas = $('.konvajs-content canvas')[0];
+      const dataUrl = canvas.toDataURL();
+      var image = new Image();
+      image.onload = function () {
+        ctx.drawImage(image, 0, 0);
+      };
+      image.src = dataUrl;
+      lc.saveShape(
+        LC.createShape('Image', { x: 0, y: 0, image: image, scale: 1 })
+      );
+      showAnchors();
+    }
+  });
+
+  // lc.respondToSizeChange();
+  //   var newImage = new Image();
+  //   newImage.src = './assets/images/clean.svg';
+
+  // $('#upload-photo').change(function (event) {
+  //   const addImage = new Image();
+
+  //   const url = URL.createObjectURL(event.target.files[0]);
+  //   if (url) {
+  //     addImage.src = url;
+  //     addImage.id = 'add_image';
+
+  //     // const xDraggables = xElem.drag();
+
+  //     // LC.util.addImageOnload(addImage, function (img) {
+  //     //   lc.saveShape(
+  //     //     LC.createShape('Image', { x: 0, y: 0, image: addImage, scale: 0.5 })
+  //     //   );
+  //     // });
+
+  //     //
+  //   }
+  // });
+
+  var save = function () {
+    localStorage.setItem('drawing', JSON.stringify(lc.getSnapshot()));
+  };
+
+  lc.on('drawingChange', save);
+  lc.on('pan', save);
+  lc.on('zoom', save);
+
+  $('#open-image').click(function () {
+    var win = window.open();
+    const base64URL = lcCanvas().toDataURL();
+    win.document.write(
+      '<iframe src="' +
+        base64URL +
+        '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
+    );
+  });
+
+  $('#clear-lc').click(function () {
+    lc.clear();
+  });
+
+  $('#undo-lc').click(function () {
+    lc.undo();
+  });
+
+  $('#redo-lc').click(function () {
+    lc.redo();
+  });
+
+  // Set up our own tools...
+  tools = [
+    {
+      name: 'pencil',
+      el: document.getElementById('tool-pencil'),
+      tool: new LC.tools.Pencil(lc),
+    },
+    {
+      name: 'eraser',
+      el: document.getElementById('tool-eraser'),
+      tool: new LC.tools.Eraser(lc),
+    },
+    {
+      name: 'text',
+      el: document.getElementById('tool-text'),
+      tool: new LC.tools.Text(lc),
+    },
+    {
+      name: 'line',
+      el: document.getElementById('tool-line'),
+      tool: new LC.tools.Line(lc),
+    },
+    {
+      name: 'arrow',
+      el: document.getElementById('tool-arrow'),
+      tool: (function () {
+        arrow = new LC.tools.Line(lc);
+        arrow.hasEndArrow = true;
+        return arrow;
+      })(),
+    },
+    {
+      name: 'dashed',
+      el: document.getElementById('tool-dashed'),
+      tool: (function () {
+        dashed = new LC.tools.Line(lc);
+        dashed.isDashed = true;
+        return dashed;
+      })(),
+    },
+    {
+      name: 'ellipse',
+      el: document.getElementById('tool-ellipse'),
+      tool: new LC.tools.Ellipse(lc),
+    },
+    {
+      name: 'tool-rectangle',
+      el: document.getElementById('tool-rectangle'),
+      tool: new LC.tools.Rectangle(lc),
+    },
+    {
+      name: 'tool-polygon',
+      el: document.getElementById('tool-polygon'),
+      tool: new LC.tools.Polygon(lc),
+    },
+    {
+      name: 'tool-select',
+      el: document.getElementById('tool-select'),
+      tool: new LC.tools.SelectShape(lc),
+    },
+    {
+      name: 'tool-pan',
+      el: document.getElementById('tool-pan'),
+      tool: new LC.tools.Pan(lc),
+    },
+  ];
+
+  strokeWidths = [
+    {
+      name: 10,
+      el: document.getElementById('sizeTool-1'),
+      size: 10,
+    },
+    {
+      name: 20,
+      el: document.getElementById('sizeTool-2'),
+      size: 20,
+    },
+    {
+      name: 50,
+      el: document.getElementById('sizeTool-3'),
+      size: 50,
+    },
+  ];
+
+  colors = [
+    {
+      name: 'black',
+      el: document.getElementById('colorTool-black'),
+      color: '#000000',
+    },
+    {
+      name: 'blue',
+      el: document.getElementById('colorTool-blue'),
+      color: '#0000ff',
+    },
+    {
+      name: 'red',
+      el: document.getElementById('colorTool-red'),
+      color: '#ff0000',
+    },
+  ];
+
+  setCurrentByName = function (ary, val) {
+    ary.forEach(function (i) {
+      $(i.el).toggleClass('current', i.name == val);
+    });
+  };
+
+  findByName = function (ary, val) {
+    var vals;
+    vals = ary.filter(function (v) {
+      return v.name == val;
+    });
+    if (vals.length == 0) return null;
+    else return vals[0];
+  };
+
+  // Wire tools
+  tools.forEach(function (t) {
+    $(t.el).click(function () {
+      var sw;
+
+      lc.setTool(t.tool);
+      setCurrentByName(tools, t.name);
+      setCurrentByName(strokeWidths, t.tool.strokeWidth);
+      $('#tools-sizes').toggleClass('disabled', t.name == 'text');
+    });
+  });
+  setCurrentByName(tools, tools[0].name);
+
+  // Wire Stroke Widths
+  // NOTE: This will not work until the stroke width PR is merged...
+  strokeWidths.forEach(function (sw) {
+    $(sw.el).click(function () {
+      lc.trigger('setStrokeWidth', sw.size);
+      setCurrentByName(strokeWidths, sw.name);
+    });
+  });
+  setCurrentByName(strokeWidths, strokeWidths[0].name);
+
+  // Wire Colors
+  colors.forEach(function (clr) {
+    $(clr.el).click(function () {
+      lc.setColor('primary', clr.color);
+      setCurrentByName(colors, clr.name);
+    });
+  });
+  setCurrentByName(colors, colors[0].name);
+};
+
+$(document).ready(function () {
+  // disable scrolling on touch devices so we can actually draw
+  $(document).bind('touchmove', function (e) {
+    if (e.target === document.documentElement) {
+      return e.preventDefault();
+    }
+  });
+  showLC();
+});
+
+$('#hide-lc').click(function () {
+  if (lc) {
+    lc.teardown();
+    lc = null;
+  }
+});
+
+$('#show-lc').click(function () {
+  if (!lc) {
+    showLC();
+  }
+});
+
+// methods
---
+++ assets/js/image-resize.js
@@ -0,0 +1,148 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+function update(activeAnchor) {
+  var group = activeAnchor.getParent();
+
+  var topLeft = group.get('.topLeft')[0];
+  var topRight = group.get('.topRight')[0];
+  var bottomRight = group.get('.bottomRight')[0];
+  var bottomLeft = group.get('.bottomLeft')[0];
+  var image = group.get('Image')[0];
+
+  var anchorX = activeAnchor.getX();
+  var anchorY = activeAnchor.getY();
+
+  // update anchor positions
+  switch (activeAnchor.getName()) {
+    case 'topLeft':
+      topRight.y(anchorY);
+      bottomLeft.x(anchorX);
+      break;
+    case 'topRight':
+      topLeft.y(anchorY);
+      bottomRight.x(anchorX);
+      break;
+    case 'bottomRight':
+      bottomLeft.y(anchorY);
+      topRight.x(anchorX);
+      break;
+    case 'bottomLeft':
+      bottomRight.y(anchorY);
+      topLeft.x(anchorX);
+      break;
+  }
+
+  image.position(topLeft.position());
+
+  var width = topRight.getX() - topLeft.getX();
+  var height = bottomLeft.getY() - topLeft.getY();
+  if (width && height) {
+    image.width(width);
+    image.height(height);
+  }
+}
+function addAnchor(group, x, y, name, anchorRadius) {
+  var stage = group.getStage();
+  var layer = group.getLayer();
+
+  var anchor = new Konva.Circle({
+    x: x,
+    y: y,
+    stroke: '#666',
+    fill: '#ddd',
+    strokeWidth: 2,
+    radius: anchorRadius || 8,
+    name: name,
+    draggable: true,
+    dragOnTop: false,
+  });
+
+  anchor.on('dragmove', function () {
+    update(this);
+    layer.draw();
+  });
+  anchor.on('mousedown touchstart', function () {
+    group.draggable(false);
+    this.moveToTop();
+  });
+  anchor.on('dragend', function () {
+    group.draggable(true);
+    layer.draw();
+  });
+  // add hover styling
+  anchor.on('mouseover', function () {
+    var layer = this.getLayer();
+    document.body.style.cursor = 'pointer';
+    this.strokeWidth(4);
+    layer.draw();
+  });
+
+  anchor.on('mouseout', function () {
+    var layer = this.getLayer();
+    document.body.style.cursor = 'default';
+    this.strokeWidth(2);
+    layer.draw();
+  });
+
+  group.add(anchor);
+}
+
+var stage = new Konva.Stage({
+  container: 'container',
+  width: width,
+  height: height,
+});
+
+var layer = new Konva.Layer();
+stage.add(layer);
+
+
+var yodaImg = new Konva.Image({
+  width: 93,
+  height: 104,
+});
+
+
+
+var yodaGroup = new Konva.Group({
+  x: 20,
+  y: 110,
+  draggable: true,
+});
+layer.add(yodaGroup);
+yodaGroup.add(yodaImg);
+addAnchor(yodaGroup, 0, 0, 'topLeft');
+addAnchor(yodaGroup, 93, 0, 'topRight');
+addAnchor(yodaGroup, 93, 104, 'bottomRight');
+addAnchor(yodaGroup, 0, 104, 'bottomLeft');
+
+function showAnchors() {
+  yodaGroup.find('Circle').show();
+  layer.draw();
+}
+
+function hideAnchors() {
+  yodaGroup.find('Circle').hide();
+  layer.draw();
+}
+
+yodaGroup.on('mouseenter', showAnchors);
+yodaGroup.on('mouseleave', hideAnchors);
+
+
+var imgObj = new Image();
+imgObj.onload = function () {
+  yodaImg.image(imgObj);
+  layer.draw();
+};
+
+if (!imgObj.src) {
+  $('#container').hide();
+}
+
+$('#upload-photo').change(function (e) {
+  const url = URL.createObjectURL(e.target.files[0]);
+  imgObj.src = url;
+  $('#container').show();
+});
